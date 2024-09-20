816d2abcf57322a445e16595db2de419fb6e7f0b
Herick2D <contato.herickmoreira@gmail.com>
2024-09-20 11:30:05 -0300

    build: ultra-car-api

---
+++ src/main/java/com/herick/ultracarapi/UltracarapiApplication.java
@@ -0,0 +1,13 @@
+package com.herick.ultracarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltracarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltracarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herick/ultracarapi/UltracarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herick.ultracarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltracarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
